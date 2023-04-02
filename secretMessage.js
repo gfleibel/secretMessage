e2ae7749ec608aef6b8d29bf4a7b22748e0cb678
Gabriel Leibel <99811494+gfleibel@users.noreply.github.com>
2023-04-02 11:44:58 -0300

    Create secretMessage.js

---
+++ secretMessage.js
@@ -0,0 +1,28 @@
+let secretMessage = ['Learning','is','not','about','what','you','get','easily','the','first','time','it','is','about','what','you','can','figure','out.','-2015, ','Chris','Pine, ','Learn','JavaScript'];
+
+//Use an array method to remove the last string of the array
+secretMessage.pop();
+//console.log(secretMessage);
+
+//Use an array method to add the words 'to'and 'Program' as separate strings to the end of the secretMessage array.
+secretMessage.push('to', 'Program');
+//console.log(secretMessage);
+
+//Change the word 'easily' to the word 'right' by accessing the index and replacing it.
+secretMessage[secretMessage.indexOf('easily')] = 'right'
+//console.log(secretMessage);
+
+//Use an array method to remove the first string of the array.
+secretMessage.shift();
+//console.log(secretMessage);
+
+//Use an array method to add the string 'Programming' to the beginning of the array.
+secretMessage.unshift('Programming');
+//console.log(secretMessage);
+
+//Use an array method to remove the strings 'get', 'right', 'the', 'first', 'time' and replace them with the singles string 'know, '.
+secretMessage.splice(secretMessage.indexOf('get'),5,'know, ');
+console.log(secretMessage);
+
+//On one line, use console.log() and .join() to print the secret message as a sentence. 
+console.log(secretMessage.join(" "));
